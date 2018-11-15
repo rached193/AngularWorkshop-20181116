0d80fc49785ee52d6eae5adaa451acc340e88c70
rached193 <goldensun193>
2018-11-15 18:16:05 +0100

    Revision, enunciados ejercicidios

---
+++ 04_flow_events/exercises-RxJS/subjet.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+var rxjs_1 = require("rxjs");
+var subject = new rxjs_1.Subject();
+subject.subscribe({
+    next: function (v) { return console.log('observerA: ' + v); }
+});
+subject.subscribe({
+    next: function (v) { return console.log('observerB: ' + v); }
+});
+var observable = rxjs_1.Observable.from([1, 2, 3]);
+observable.subscribe(subject);
+var subject_ramdom = new rxjs_1.Subject();
+// subscriber 1
+subject.subscribe(function (data) {
+    console.log(data); // 0.24957144215097515 (random number)
+});
+// subscriber 2
+subject.subscribe(function (data) {
+    console.log(data); // 0.24957144215097515 (random number)
+});
+subject.next(Math.random());
